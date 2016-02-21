ad1bc4d9f9436c70aba98937b227529ecef4eb9f
Mahendra Kalkura <mahendra@kalkura.com>
2016-02-21 15:50:44 +0530

    references_management.js

--- references_management.js
+++ references_management.js
@@ -1,1 +1,19 @@
-jQuery(function () {});
+jQuery(function () {
+    jQuery('.references_management_widget').on('click', '.search', function () {
+        var table_1 = jQuery(this).parents('table');
+        var keywords = table_1.find('[name="references_management_4_keywords"]').val().toLowerCase();
+        var table_2 = table_1.next().find('table');
+        jQuery.each(table_2.find('tr.article'), function () {
+            var $this = jQuery(this);
+            if (keywords === '' || $this.find('td').eq(1).text().toLowerCase().indexOf(keywords) !== -1) {
+                $this.removeClass('hide');
+            } else {
+                $this.addClass('hide');
+            }
+        });
+        table_2.find('tr.article').removeClass('even');
+        table_2.find('tr.article').removeClass('odd');
+        table_2.find('tr.article:not(.hide):even').addClass('even');
+        table_2.find('tr.article:not(.hide):odd').addClass('odd');
+    });
+});
