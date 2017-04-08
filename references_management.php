a6eb3fe95b6e02acfdfd01e37f542824b8237f97
Mahendra Kalkura <mahendra@kalkura.com>
2017-04-08 12:42:18 +0530

    references_management.php

--- references_management.php
+++ references_management.php
@@ -863,7 +863,14 @@ ALTER TABLE `%sarticles_authors`
     ON DELETE CASCADE
     ON UPDATE CASCADE;
 EOD;
-    $GLOBALS['wpdb']->query(sprintf($query, references_management_get_prefix(), references_management_get_prefix(), references_management_get_prefix()));
+    $GLOBALS['wpdb']->query(
+        sprintf(
+            $query,
+            references_management_get_prefix(),
+            references_management_get_prefix(),
+            references_management_get_prefix()
+        )
+    );
 
     $query = <<<EOD
 ALTER TABLE `%sarticles_authors`
