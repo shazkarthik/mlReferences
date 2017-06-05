8d047da2aef3734c42d5b0569a78beb9846197c8
Mahendra Kalkura <mahendra@kalkura.com>
2017-06-05 23:29:27 +0530

    modules/actions.php

--- modules/actions.php
+++ modules/actions.php
@@ -3,10 +3,9 @@
 function mlReferences_actions_reset()
 {
     $documents = mlReferences_models_documents_select_all();
-    if (empty($documents)) {
-        return;
-    }
-    foreach ($documents as $document) {
-        mlReferences_models_documents_delete($document->id);
+    if (!empty($documents)) {
+        foreach ($documents as $document) {
+            mlReferences_models_documents_delete($document['id']);
+        }
     }
 }
